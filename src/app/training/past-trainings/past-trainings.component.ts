4a7ec1d0651ef51c27f01af259ed86fee865f9bf
manojk08 <kalemanoj3@gmail.com>
2020-11-15 09:25:43 +0530

    update all

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -7,7 +7,7 @@ import { TrainingService } from '../training.service';
 @Component({
   selector: 'app-past-trainings',
   templateUrl: './past-trainings.component.html',
-  styleUrls: ['./past-trainings.component.css']
+  styleUrls: ['./past-trainings.component.css'],
 })
 export class PastTrainingsComponent implements OnInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
@@ -15,7 +15,7 @@ export class PastTrainingsComponent implements OnInit {
 
   constructor(private trainingService: TrainingService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
   }
 }
